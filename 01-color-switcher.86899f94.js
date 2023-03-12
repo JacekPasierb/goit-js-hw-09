8de3e8646a58f35a54ff2a02d0588039426d54d4
JacekPasierb <113291374+JacekPasierb@users.noreply.github.com>
2023-03-12 22:42:44 +0000

    Deploying to gh-pages from @ JacekPasierb/goit-js-hw-09@1747949f1931c59a58111637874991fa2dfdf79d 🚀

---
+++ 01-color-switcher.86899f94.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=0;e.addEventListener("click",(()=>{e.disabled=!0,a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.86899f94.js.map
---
+++ 01-color-switcher.a5d6d0a4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=0;t.addEventListener("click",(function(){t.disabled=!0,o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.a5d6d0a4.js.map
