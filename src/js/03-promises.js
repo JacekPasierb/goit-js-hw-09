d08ddd3483b1b8d2068740a73bf019eb184feb38
Jacek Pasierb <jack_byk@o2.pl>
2023-03-12 23:58:41 +0100

    Creatde code for task third, all code beacuse created new repo

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector('input[name=delay]');
+const step = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount]');
+
+const form = document.querySelector('.form');
+let timerId = null;
+
+form.addEventListener('submit', e => {
+  // deklaracja zmiennych
+  let numDelay = delay.value;
+  let numPromise = amount.value;
+  let numStep = step.value;
+  // deklaracja zmiennych
+
+  // utworzenie funkcji
+  function createPromise(position, delay) {
+    timerId = setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        ); // Fulfill
+      } else {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        ); // Reject
+      }
+    }, numDelay);
   }
-}
+  // utworzenie funkcji
+
+  // działanie na submit
+  e.preventDefault();
+  clearTimeout(timerId);
+
+  for (let i = 0; i < numPromise; i++) {
+    let position = i;
+    if (i == 0) {
+      createPromise(position, numDelay);
+    } else {
+      numDelay = numStep;
+      createPromise(position, numDelay);
+    }
+  }
+
+  form.reset();
+});
